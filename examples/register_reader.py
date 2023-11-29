2a4a7bfbce1f6cac923cef04e7d890a23cbd12df
Torbjørn Østmoe <torbjorn.ostmoe@ideas.no>
2023-11-29 15:58:57 +0100

    Removing the virtual register stuff.. Packet should be accessed directly as described by the "IDEAS Readout and Control Packet Protocol Reference".

--- examples/register_reader.py
+++
@@ -1,6 +0,0 @@
-
-import pandas as pd
-
-register = pd.read_json('nm2_asic_conf.json')
-data = register['consolidated_model'][0]
-print(data)
--- examples/top_level.py
+++ examples/top_level.py
@@ -29,15 +29,21 @@ SPI_REG0_RESERVED		=	0
 
 tcp.writeAsicSpiRegister(SPI_REG0, 1, 5, 5)
 data = tcp.getSystemReadBack(200)
-print(f'SPI write ex: {data}')
+print(f'SPI Reg 0: {data}')
 
 tcp.readAsicSpiExRegister(SPI_REG0, 5)
 data = tcp.getSystemReadBack(200)
-print(f'SPI write ex: {data}')
+print(f'SPI Reg 0: {data}')
 
 SPI_REG1_CLK_DIV_MODE	=	2
 SPI_REG1_SYSCLK_DLY		=	2
 SPI_REG1_PLL_ENABLE		=	1
 SPI_REG1_RESERVED		=	0
 
-# tcp.writeAsicSpiRegister(SPI_REG1, 1, 5, 5)
+tcp.writeAsicSpiRegister(SPI_REG1, 1, 7, 26)
+data = tcp.getSystemReadBack(200)
+print(f'SPI Reg 1: {data}')
+
+tcp.readAsicSpiExRegister(SPI_REG1, 7)
+data = tcp.getSystemReadBack(200)
+print(f'SPI Reg 1: {data}')
