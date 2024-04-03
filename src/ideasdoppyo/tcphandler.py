6dd8edb79c8858478f91b7b914638c72ae098da8
Torbjørn  Østmoe <torbjorn.ostmoe@ideas.no>
2024-04-03 13:46:03 +0200

    debug

--- src/ideasdoppyo/tcphandler.py
+++ src/ideasdoppyo/tcphandler.py
@@ -141,7 +141,9 @@ class TCPhandler:
         while len(data) < expected_data_length:
             data += self.tcp_s.recv(expected_data_length)
         spare_bytes_length = len(data) - expected_data_length
+        print(f'FOR DEBUG - spare_bytes_length: {spare_bytes_length}')
         self.spare_bytes = data[-spare_bytes_length:]
+        print(f'FOR DEBUG - spare_bytes: {spare_bytes_length}')
         if self.doPrint:
             self.doPrinter.data_bytes = data[:-spare_bytes_length]
             print(self.doPrinter)
